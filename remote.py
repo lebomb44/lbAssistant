e194bd5dfde76ba510f740d5cf7cc38748ffa703
LeBomb <cambon.olivier@gmail.com>
2023-03-26 22:03:50 +0200

    Increase timeout. This was too short.

--- remote.py
+++ remote.py
@@ -6,6 +6,7 @@
 import subprocess
 import requests
 import urllib3
+import time
 
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -19,7 +20,7 @@ def checkdisk(target, target_name, user, path, level=80):
                                 stdout=subprocess.PIPE,
                                 stderr=subprocess.PIPE,
                                 check=False,
-                                timeout=2.0)
+                                timeout=10.0)
         if result.returncode == 0:
             resp_array = result.stdout.decode('utf-8').split()
             if len(resp_array) == 15:
@@ -27,14 +28,14 @@ def checkdisk(target, target_name, user, path, level=80):
                 if rate < level:
                     return ""
                 else:
-                    return "Disque plein a " + str(rate) + "% sur " + target_name
+                    return "Disque plein a " + str(rate) + "% sur " + target_name + " a " + time.strftime('%Y/%m/%d %H:%M:%S: ')
             else:
-                return "Longueur incorrecte a " + str(len(resp_array)) + " pour le test disque sur " + target_name
+                return "Longueur incorrecte a " + str(len(resp_array)) + " pour le test disque sur " + target_name + " a " + time.strftime('%Y/%m/%d %H:%M:%S: ')
         else:
-            return "Code retour incorrect a " + str(int(result.returncode)) + " pour le test disque sur " + target_name
+            return "Code retour incorrect a " + str(int(result.returncode)) + " pour le test disque sur " + target_name + " a " + time.strftime('%Y/%m/%d %H:%M:%S: ')
     except Exception as ex:
-        print("ERROR: Exception checkdisk: " + str(ex))
-        return "Exception lors du test disque sur " + target_name
+        print("ERROR " + time.strftime('%Y/%m/%d %H:%M:%S: ') + ": Exception checkdisk: " + str(ex))
+        return "Exception lors du test disque sur " + target_name + " a " + time.strftime('%Y/%m/%d %H:%M:%S: ')
 
 def checkhttp(url, url_name, answer):
     """ Check correct execution of HTTP request """
@@ -44,9 +45,10 @@ def checkhttp(url, url_name, answer):
             if answer == req.text:
                 return ""
             else:
-                return "Reponse incorrecte a " + req.text + " pour le test d'acces sur " + url_name
+                return "Reponse incorrecte a " + req.text + " pour le test d'acces sur " + url_name + " a " + time.strftime('%Y/%m/%d %H:%M:%S: ')
         else:
-            return "Code retour incorrect a " + str(req.status_code) + " pour le test d'acces sur " + url_name
+            return "Code retour incorrect a " + str(req.status_code) + " pour le test d'acces sur " + url_name + " a " + time.strftime('%Y/%m/%d %H:%M:%S: ')
     except Exception as ex:
-        print("ERROR: Exception checkhttp: " + str(ex))
-        return "Exception lors du test d'acces sur " + url_name
+        print("ERROR " + time.strftime('%Y/%m/%d %H:%M:%S: ') + ": Exception checkhttp: " + str(ex))
+        return "Exception lors du test d'acces sur " + url_name + " a " + time.strftime('%Y/%m/%d %H:%M:%S: ')
+
